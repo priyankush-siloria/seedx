b9cd11b045666186ecbbf38af2777e14c455b77e
Priyankush Salouria <pksalo6@gmail.com>
2023-04-28 12:26:21 +0530

    Fix runserver command in docker config

--- seed_x/createsuperuser.py
+++ seed_x/createsuperuser.py
@@ -1,6 +1,5 @@
 import os
 
-from django.db import IntegrityError
 from django.contrib.auth.models import User
 
 env = os.environ
@@ -10,7 +9,5 @@ try:
         username=env["ADMIN_USERNAME"],
         email=env["ADMIN_EMAIL"],
         password=env["ADMIN_PASSWORD"])
-except IntegrityError:
-    print(f"Super User with username {env('SUPER_USER_NAME')} is already exit!")
 except Exception as e:
-    print(e)
+    print("Superuser already exists...")
